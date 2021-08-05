8427d3a71be88014615febec526eb08bbfbfb361
Ethan <disdroido@gmail.com>
2021-08-05 15:23:47 +1000

    removed 2 migrations for heroku

--- db/migrate/20210804134339_add_columns_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :current_sign_in_at, :datetime
-    add_column :users, :last_sign_in_at, :datetime
-    add_column :users, :current_sign_in_ip, :string
-    add_column :users, :last_sign_in_ip, :string
-  end
-end
