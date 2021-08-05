0c929eca271990dfa95a5c54a2473c9bedb4befd
Ethan <disdroido@gmail.com>
2021-08-05 22:55:39 +1000

    user model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   private
 
   def set_default_role
-    self.update(role_id: Role.where(code: 'default').id)
+    self.update(role_id: Role.find_by(code: 'default').id)
   end
 end
