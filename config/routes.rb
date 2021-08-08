c7c0c9afa75d80d92cd853e7e2775a540f4e1e49
Ethan <disdroido@gmail.com>
2021-08-08 23:25:20 +1000

    changed navigation, changed routes, added edit and delete features for user listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   resources :users, only: [:index, :show, :edit, :update]
   resources :roles
   resources :listings
   root to: "home#index"
 
   devise_scope :user do
-    get '/logout', to: 'devise/sessions#destroy', as: :logout
-  end  
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+    delete "sign_out", to: "devise/sessions#destroy"
+  end
 end
