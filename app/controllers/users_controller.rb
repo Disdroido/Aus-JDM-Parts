259f6e20b77e68e579feaca1352b52011ee9c273
Ethan <disdroido@gmail.com>
2021-08-10 15:02:03 +1000

    heroku changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @listings
   end
 
   def edit
