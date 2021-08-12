21f510b970fe465cd1fdb422f951b75838cd1714
Ethan <disdroido@gmail.com>
2021-08-12 16:40:12 +1000

    assign checkout to current user

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,19 +3,20 @@ class CheckoutController < ApplicationController
   def create
     listing = Listing.find(params[:id])
     @session = Stripe::Checkout::Session.create({
-      payment_method_types: ['card'],
-      line_items: [{
-        name: listing.name,
-        amount: listing.retail_price,
-        currency: "usd",
-        quantity: 1
-      }],
-      mode: 'payment',
-      success_url: root_url,
-      cancel_url: root_url,
+        customer: current_user.stripe_customer_id,
+        payment_method_types: ['card'],
+        line_items: [{
+            name: listing.name,
+            amount: listing.retail_price,
+            currency: "aud",
+            quantity: 1
+        }],
+        mode: 'payment',
+        success_url: root_url,
+        cancel_url: root_url,
     });
     respond_to do |format|
-      format.js
+        format.js
     end
   end
 
